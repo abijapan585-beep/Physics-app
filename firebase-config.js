60c615f2d856ef425a3a632588eb2f7279afe4bf
abijapan585-beep <abijapan585@gmail.com>
2026-08-12 08:10:52 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCK3I1tGdj5StuzT9OELBKDrIcDJMPLE18",
+  authDomain: "physics-app-57fd7.firebaseapp.com",
+  projectId: "physics-app-57fd7",
+  storageBucket: "physics-app-57fd7.firebasestorage.app",
+  messagingSenderId: "66529385856",
+  appId: "1:66529385856:web:867e58a04fd5cc42903565"
+};
+
+firebase.initializeApp(firebaseConfig);
